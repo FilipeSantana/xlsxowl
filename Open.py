3f105d054257ac3e950479bd46626731c48988ac
Filipe Santana <filipe.santana.silva@gmail.com>
2018-02-16 09:28:14 -0200

    Begin
    base files with the following capabilities:
    - Loading a xslx spreadsheet;
    - Printing spreadsheets names;
    - Printing headers of a specific spreadsheet; and
    - Counting the number of spreadsheets available.

---
+++ Open.py
@@ -0,0 +1,13 @@
+import xlrd
+
+book = xlrd.open_workbook('spreadsheets/Data.xlsx')
+sheet = book.sheet_by_name('final-data')
+print("The number of worksheets is {0}".format(book.nsheets))
+print("Worksheet name(s): {0}".format(book.sheet_names()))
+#sh = book.sheet_by_index(0)
+#print("{0} {1} {2}".format(sh.name, sh.nrows, sh.ncols))
+#print("Cell D30 is {0}".format(sh.cell_value(rowx=29, colx=3)))
+#for rx in range(sh.nrows):
+    #print(sh.row(rx))
+
+print("The sheet",sheet.name,"has as headings: ",sheet.row_values(0))
